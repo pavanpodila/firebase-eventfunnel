7d36caa3bb85504b715d705c19ee626d4df1c226
pavanpodila <pavan.podila@gmail.com>
2019-03-12 17:01:22 +0530

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core';
+
+class App extends React.Component {
+  render() {
+    return <Button />;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
